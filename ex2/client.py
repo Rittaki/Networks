6fdc074b10cde58d49f39ad9b6f249b83fbde55c
rittaki <com.margo@gmail.com>
2021-11-24 00:00:39 +0200

    stam updates

--- ex2/client.py
+++ ex2/client.py
@@ -1,10 +1,46 @@
 import socket
+import sys
+import time
+import os
+import random
+import string
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+SERVER_IP = sys.argv[1]
+SERVER_PORT = int(sys.argv[2])
+DIR_PATH = sys.argv[3]
+PERIOD = sys.argv[4]
+try:
+    IDENTIFY = sys.argv[5]
+    if len(IDENTIFY) < 128:
+        exit()
+except:
+    IDENTIFY = None
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 12345))
-s.send(b'hello')
+s.connect((SERVER_IP, SERVER_PORT))
 
-data = s.recv(100)
-print("Server sent: ", data)
+class Watcher:
+    def __init__(self, path, socket):
+        self.observer = Observer()
 
-s.close()
+    def run(self):
+        event_handler = EventHandler()
+
+class EventHandler(FileSystemEventHandler):
+    def __init__(self, path, socket):
+        self.observer = Observer()
+
+if __name__ == '__main__':
+    watch = Watcher(DIR_PATH, s)
+    watch.run()
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect((SERVER_IP, SERVER_PORT))
+# s.send(b'hello')
+#
+# data = s.recv(100)
+# print("Server sent: ", data)
+#
+# s.close()
--- ex2/server.py
+++ ex2/server.py
@@ -1,17 +1,25 @@
 import socket
+import sys
+import time
+import os
+import watchdog
+import random
+import string
+
+CLIENT_PORT = int(sys.argv[1])
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('', 12345))
 server.listen(5)
 
 while True:
-	client_socket, client_address = server.accept()
-	print('Connection from: ', client_address)
-	
-	data = client_socket.recv(100)
-	print('Received: ', data)
-	
-	client_socket.send(data.upper())
-	
-	client_socket.close()
-	print('Client disconnected')
+    client_socket, client_address = server.accept()
+    print('Connection from: ', client_address)
+
+    data = client_socket.recv(100)
+    print('Received: ', data)
+
+    client_socket.send(data.upper())
+
+    client_socket.close()
+    print('Client disconnected')
