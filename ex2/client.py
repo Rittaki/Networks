8e5fbc0fd34ccc8a006b89121b25d89afa36b5c1
rittaki <com.margo@gmail.com>
2021-11-24 23:48:17 +0200

    TO DO: finish new user

--- ex2/client.py
+++ ex2/client.py
@@ -7,32 +7,71 @@ import string
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-SERVER_IP = sys.argv[1]
-SERVER_PORT = int(sys.argv[2])
+CLIENT_IP = sys.argv[1]
+PORT = int(sys.argv[2])
 DIR_PATH = sys.argv[3]
 PERIOD = sys.argv[4]
-try:
-    IDENTIFY = sys.argv[5]
-    if len(IDENTIFY) < 128:
-        exit()
-except:
-    IDENTIFY = None
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((SERVER_IP, SERVER_PORT))
 
 class Watcher:
-    def __init__(self, path, socket):
+    def __init__(self, path, client):
         self.observer = Observer()
+        self.path = path
+        self.client = client
 
     def run(self):
-        event_handler = EventHandler()
+        event_handler = EventHandler(self.path, self.client)
+        self.observer.schedule(event_handler, self.path, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except KeyboardInterrupt:
+            self.observer.stop()
+        self.observer.join()
+        self.client.stop()
 
 class EventHandler(FileSystemEventHandler):
-    def __init__(self, path, socket):
-        self.observer = Observer()
+    def __init__(self, path, client):
+        # self.observer = Observer()
+        self.path = path
+        self.client = client
+
+    # on_any_event function - delete later
+    def on_any_event(self, event):
+        if event.is_directory:
+            print("is directory")
+        if event.event_type == 'created':
+            print("created")
+        if event.event_type == 'modified':
+            print("modified")
+        if event.event_type == 'moved':
+            print("moved")
+        if event.event_type == 'deleted':
+            print("deleted")
+
+    def on_created(self, event):
+        pass
+
+    def on_modified(self, event):
+        pass
+
+    def on_moved(self, event):
+        pass
+
+    def on_deleted(self, event):
+        pass
+
+def create_new_client():
+    pass
 
 if __name__ == '__main__':
+    # creating new socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((CLIENT_IP, PORT))
+    try:
+        IDENTIFY = sys.argv[5]
+    except:
+        s.send("new user,".encode('utf-8') + DIR_PATH.encode('utf-8'))
     watch = Watcher(DIR_PATH, s)
     watch.run()
 
--- ex2/server.py
+++ ex2/server.py
@@ -8,18 +8,22 @@ import string
 
 CLIENT_PORT = int(sys.argv[1])
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('', 12345))
-server.listen(5)
+def get_data(s):
+    data = s.recv(1024)
+    string = data.decode('utf-8')
+    if string == "new user":
+        print("nice")
+    print(string)
 
-while True:
-    client_socket, client_address = server.accept()
-    print('Connection from: ', client_address)
-
-    data = client_socket.recv(100)
-    print('Received: ', data)
-
-    client_socket.send(data.upper())
-
-    client_socket.close()
-    print('Client disconnected')
+if __name__ == "__main__":
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('', CLIENT_PORT))
+    server.listen(5)
+    while True:
+        client_socket, client_address = server.accept()
+        print('Connection from: ', client_address)
+        get_data(client_socket)
+        # data = client_socket.recv(100)
+        # print('Received: ', data)
+        client_socket.close()
+        print('Client disconnected')
